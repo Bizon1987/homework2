41fff3d8609fae3c4d8af22c0a835bf89948dc33
Vladimir Khananov <vladimirkhananov4@gmail.com>
2025-08-09 12:13:56 +0500

    Changes commit

--- main.homework3.php
+++ main.homework3.php
@@ -37,3 +37,26 @@ if($testFunc5 == true) {
     echo "В переменной не содержится null" . PHP_EOL;
 }
 
+switch (true) {
+    case is_int($variable):
+        $type = 'int';
+        break;
+    case is_float($variable):
+        $type = 'float';
+        break;
+    case is_bool($variable):
+        $type = 'bool';
+        break;
+    case is_string($variable):
+        $type = 'string';
+        break;
+    case is_null($variable):
+        $type = 'null';
+        break;
+    default:
+        $type = 'неизвестный тип';
+}
+
+echo "В переменной содержится $type" . PHP_EOL;
+
+
