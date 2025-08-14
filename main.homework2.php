93412e7b92d384acf7e6706245c2dfd2105b955c
Vladimir Khananov <vladimirkhananov4@gmail.com>
2025-08-14 19:23:43 +0500

    New commit

--- main.homework2.php
+++ main.homework2.php
@@ -1,5 +1,21 @@
 <?php
-$a='Рыба';
-$b='человек';
 
-echo $a . " рыбою сыта, а " .  $b . " человеком";
+$input1 = trim(fgets(STDIN));
+$input2 = trim(fgets(STDIN));
+
+if (filter_var($input1, FILTER_VALIDATE_INT) === false || filter_var($input2, FILTER_VALIDATE_INT) === false) {
+    fwrite(STDERR, "Введите, пожалуйста, число\n");
+    exit(1);
+}
+
+$num1 = (int)$input1;
+$num2 = (int)$input2;
+
+if ($num2 === 0) {
+    fwrite(STDERR, "Делить на 0 нельзя\n");
+    exit(1);
+}
+
+$result = $num1 / $num2;
+
+echo $result . PHP_EOL;
